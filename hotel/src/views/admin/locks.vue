84488d194005beb5e29afc39c15181118294c622
Casper Van Damme <casper.van.damme@student.howest.be>
2024-07-14 14:19:20 +0200

    quick fix

--- hotel/src/views/admin/locks.vue
+++ hotel/src/views/admin/locks.vue
@@ -1,26 +1,25 @@
 <template>
     <div class="w-screen h-screen grid justify-items-center">
         <h1 class="text-5xl text-darkGreen text-center font-cambria font-normal mt-40">SMART LOCKS</h1>
-        <div class="w-full grid justify-items-center gap-8 mt-12">
-            <div class="grid grid-cols-3 h-56 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+        <div class="w-full grid justify-items-center gap-6 mt-8">
+            <div class="grid grid-cols-3 h-48 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
                 <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
-                <div class="grid justify-items-start my-2 ml-10 w-96">
+                <div class="grid justify-items-start my-2 ml-6 w-96">
                     <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom deluxe</h2>
-                    <p class="text-2xl text-darkGreen font-cambria -mt-8">Last lock change:</p>
-                    <p class="text-2xl text-darkGreen font-cambria -mt-12">DATETIME - Person</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-10">Last lock change:</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-10">DATETIME - Person</p>
                 </div>
-                <img src="../../../public/icons/locked.svg" alt="" class="h-12 mt-4 ml-56">
+                <img src="../../../public/icons/locked.svg" alt="" class="h-12 mt-4 ml-60">
             </div>
-            <div class="grid grid-cols-3 h-56 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+            <div class="grid grid-cols-3 h-48 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
                 <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
-                <div class="grid justify-items-start my-2 ml-10 w-96">
+                <div class="grid justify-items-start my-2 ml-6 w-96">
                     <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom</h2>
-                    <p class="text-2xl text-darkGreen font-cambria -mt-8">Last lock change:</p>
-                    <p class="text-2xl text-darkGreen font-cambria -mt-12">DATETIME - Person</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-10">Last lock change:</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-10">DATETIME - Person</p>
                 </div>
-                <img src="../../../public/icons/unlocked.svg" alt="" class="h-12 mt-4 ml-56">
+                <img src="../../../public/icons/unlocked.svg" alt="" class="h-12 mt-4 ml-60">
             </div>
         </div>
-        
     </div>
 </template>
--- hotel/src/views/reservations.vue
+++ hotel/src/views/reservations.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="w-screen h-screen grid justify-items-center">
         <h1 class="text-5xl text-darkGreen text-center font-cambria font-normal mt-40">YOUR RESERVARTIONS</h1>
-        <div class="flex h-56 w-2/3 bg-secondary rounded-3xl justify-between">
-            <img src="../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl">
-            <div class="grid justify-items-start -ml-4 my-2">
-                <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom deluxe</h2>
-                <p class="text-2xl text-darkGreen font-cambria -mt-8">Room access until DATE</p>
-                <p class="text-2xl text-darkGreen font-cambria mt-6">Room status: UNLOCKED</p>
+        <div class="w-full grid justify-items-center gap-6 mt-8">
+            <div class="grid grid-cols-3 h-48 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+                <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
+                <div class="grid justify-items-start my-2 ml-6 w-96">
+                    <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom deluxe</h2>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-4">Room access until DATE</p>
+                    <p class="text-2xl text-darkGreen font-cambria mt-4">Room status: UNLOCKED</p>
+                </div>
+                <div class="self-center w-44 h-44 grid bg-primary p-8 rounded-3xl border-solid border-4 border-darkGreen ml-28">
+                    <img src="../../public/icons/camera.svg" alt="" class="place-self-center">
+                    <p class="text-center mt-1 font-cambria">Lock your room</p>
+                </div>
             </div>
-            <div class="self-center grid bg-primary p-8 rounded-3xl border-solid border-4 border-darkGreen mr-5">
-                <img src="../../public/icons/camera.svg" alt="" class="place-self-center">
-                <p class="text-center mt-1 font-cambria">Lock your room</p>
+            <div class="grid grid-cols-3 h-48 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+                <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
+                <div class="grid justify-items-start my-2 ml-6 w-96">
+                    <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom</h2>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-4">Room access until DATE</p>
+                    <p class="text-2xl text-darkGreen font-cambria mt-4">Room status: UNLOCKED</p>
+                </div>
+                <div class="self-center w-44 h-44 grid bg-primary p-8 rounded-3xl border-solid border-4 border-darkGreen ml-28">
+                    <img src="../../public/icons/camera.svg" alt="" class="place-self-center">
+                    <p class="text-center mt-1 font-cambria">Lock your room</p>
+                </div>
             </div>
         </div>
     </div>
+    
 </template>
