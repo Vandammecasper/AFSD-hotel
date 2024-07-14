41bf32c7369921d3f01e982d37639679ac9bdcfc
Casper Van Damme <casper.van.damme@student.howest.be>
2024-07-14 13:07:02 +0200

    smart locks visual done

--- hotel/src/views/admin/locks.vue
+++ hotel/src/views/admin/locks.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="w-screen h-screen grid justify-items-center">
         <h1 class="text-5xl text-darkGreen text-center font-cambria font-normal mt-40">SMART LOCKS</h1>
+        <div class="w-full grid justify-items-center gap-8 mt-12">
+            <div class="grid grid-cols-3 h-56 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+                <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
+                <div class="grid justify-items-start my-2 ml-10 w-96">
+                    <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom deluxe</h2>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-8">Last lock change:</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-12">DATETIME - Person</p>
+                </div>
+                <img src="../../../public/icons/locked.svg" alt="" class="h-12 mt-4 ml-56">
+            </div>
+            <div class="grid grid-cols-3 h-56 w-3/5 bg-secondary rounded-3xl gap-0 justify-between">
+                <img src="../../../public/images/hotelRoomDeluxe.jpg" alt="" class="rounded-s-2xl h-full">
+                <div class="grid justify-items-start my-2 ml-10 w-96">
+                    <h2 class="text-3xl font-bold font-cambria text-darkGreen">2 person bedroom</h2>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-8">Last lock change:</p>
+                    <p class="text-2xl text-darkGreen font-cambria -mt-12">DATETIME - Person</p>
+                </div>
+                <img src="../../../public/icons/unlocked.svg" alt="" class="h-12 mt-4 ml-56">
+            </div>
+        </div>
+        
     </div>
 </template>
