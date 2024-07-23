9ecc0004a9bc6769bb9406f3484c6050ddcf9fd3
Casper Van Damme <casper.van.damme@student.howest.be>
2024-07-23 17:34:57 +0200

    graphql working

--- hotel/packages/api/src/app.module.ts
+++ hotel/packages/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: true,
+  }),
+  RoomsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
