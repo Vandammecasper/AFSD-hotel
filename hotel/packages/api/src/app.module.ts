3f4bbd6a97b116867ca3cdccaaf2faac6a257915
Casper Van Damme <casper.van.damme@student.howest.be>
2024-07-23 18:09:34 +0200

    room entity added

--- hotel/packages/api/src/app.module.ts
+++ hotel/packages/api/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RoomsModule } from './rooms/rooms.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+        type: 'mongodb',
+        url: `mongodb://localhost:27027/api`,
+        entities: [__dirname + '/**/*.entity.{js,ts}'],
+        synchronize: true, // Careful with this in production
+        useNewUrlParser: true,
+        useUnifiedTopology: true, // Disable deprecated warnings
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: true,
--- hotel/packages/api/src/rooms/entities/room.entity.ts
+++ hotel/packages/api/src/rooms/entities/room.entity.ts
@@ -1,7 +1,45 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+import { string } from 'yargs';
 
+@Entity()
 @ObjectType()
 export class Room {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+
+  @ObjectIdColumn()
+  @Field(() => Int)
+  id: number;
+
+  @Column()
+  @Field(() => String, {description: 'The name of the room', nullable: false})
+  roomName: string;
+
+  @Column()
+  @Field(() => Int, {description: 'The number of the room', nullable: false})
+  roomNumber: number;
+
+  @Column()
+  @Field(() => Int, {description: 'Price per night in euros', nullable: false})
+  price: number;
+
+  @Column()
+  @Field(() => Int, {description: 'Maximum number of customers that can stay in that room', nullable: false})
+  maxOccupation: number;
+
+  @Column()
+  @Field(() => Int, {description: 'Size of the room in m²', nullable: false})
+  size: number;
+
+  @Column()
+  @Field(() => [String], {description: 'List of facilities', nullable: false})
+  facilities: String[];
+
+  @Column()
+  @Field(() => String, {description: 'Description of the room', nullable: false})
+  description: string;
+
+  @Column()
+  @Field(() => String, {description: 'Picture of the room', nullable: false})
+  roomPicture: string;
+
 }
